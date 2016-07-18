c26bebf86749d82b5f173a7209cf0190f1ef5027
Andrey Padiy <andreypadiy@gmail.com>
2016-07-18 12:45:07 +0100

    Testing multifile HTML out

--- jenkins.sh
+++ jenkins.sh
@@ -1,9 +1,10 @@
-#!/bin/bash -x
+#!/bin/bash
 if [ ! -d terraform_plan_report ]; then
  mkdir terraform_plan_report
 fi
 PATH=/usr/local/src/terraform:$PATH
 terraform plan > terraform.out
-cat terraform.out | ./ansi2html.sh --bg=dark > terraform_plan_report/index.html
+cat terraform.out | ./ansi2html.sh --bg=dark > terraform_plan_report/live.html
+cat terraform.out | ./ansi2html.sh --bg=dark > terraform_plan_report/dev.html
 # Strip CSS styling for PR update message
-sed '/<style type=\"text\/css\">/,/<\/style>/d' terraform_plan_report/index.html > terraform_plan_report/pr_update.md
+sed '/<style type=\"text\/css\">/,/<\/style>/d' terraform_plan_report/live.html > terraform_plan_report/pr_update.md
